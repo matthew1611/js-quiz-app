347156e7706fcdde35304ac5b2a69e5c3584ae57
Matthew <69815486+matthew1611@users.noreply.github.com>
2024-10-09 12:40:05 -0500

    add questions /answers

---
+++ script.js
@@ -0,0 +1,38 @@
+const questions = [
+  {
+    question: 'Where are you?',
+    answers: [
+      { text: 'home', correct: true },
+      { text: 'work', correct: false },
+      { text: 'coffee shop', correct: false },
+      { text: 'hospital', correct: false },
+    ],
+  },
+  {
+    question: 'How do you feel?',
+    answers: [
+      { text: 'bad', correct: false },
+      { text: 'angry', correct: false },
+      { text: 'good', correct: true },
+      { text: 'drunk', correct: false },
+    ],
+  },
+  {
+    question: 'Where were you born?',
+    answers: [
+      { text: 'home', correct: false },
+      { text: 'McHenry', correct: false },
+      { text: 'Crystal Lake', correct: false },
+      { text: 'Barrington', correct: true },
+    ],
+  },
+  {
+    question: 'What is the name of your first dog?',
+    answers: [
+      { text: 'Millie', correct: true },
+      { text: 'Penny', correct: true },
+      { text: 'Lucy', correct: false },
+      { text: 'Rex', correct: false },
+    ],
+  },
+];
